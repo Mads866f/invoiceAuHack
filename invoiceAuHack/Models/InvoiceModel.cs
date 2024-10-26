e0f26bb9085dec296a18125ed8453a871fb6e783
mads866f <mads.larsen@boostplm.com>
2024-10-26 13:33:47 +0200

    Added input form

--- invoiceAuHack/Models/InvoiceModel.cs
+++ invoiceAuHack/Models/InvoiceModel.cs
@@ -2,5 +2,11 @@ namespace invoiceAuHack.Models;
 
 public class InvoiceModel
 {
-    
+    public DateTime Date { get; set; }
+    public string InvoiceNumber { get; set; }
+    public int Amount { get; set; }
+    public string Description { get; set; }
+    public string Sender { get; set; }
+    public string Receiver { get; set; }
+    public DateTime PaymentDate { get; set; }
 }
